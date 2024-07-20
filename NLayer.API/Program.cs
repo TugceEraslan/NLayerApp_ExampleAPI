f06b01f105bb62aff2cb2b8b5841cf7d805b5fa1
Tuğçe Eraslan <tugceeraslan34@gmail.com>
2024-07-20 05:06:20 +0300

    + update database

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,3 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository;
+using NLayer.Repository.Repositories;
+using NLayer.Repository.UnitOfWorks;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/*Interfacelerin implementasyonalarýnýn da migrationa eklenmesi için aþaðýdaki iþlemleri yapýyoruz.*/
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>(); // IUnitOfWork ile karþýlaþýrsan UnitOfWork'u nesne örneði alacaksýn.
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));  // Generic olduðu için typeof olarak tanýmlýyoruz ve <> tanýmlýyoruz.
+                                                                                        // IGenericRepository<> ile karþýlaþýrsan GenericRepository<>'u nesne örneði alacaksýn
+
+//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        /* AppDbContext'i içeren katmanýn adýný al diyoruz.
+         * Yani Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name diyerek aslýnda NLayer.Repository'i alýyoruz */
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);  // AppDbContext'i içeren Assembly'nin adýný al diyoruz
+    });
+});
 
 var app = builder.Build();
 
